ecce5869a928403590387aaa3968ee032653b79f
shanks1554 <deepnagpal147514@gmail.com>
2025-05-31 16:35:38 +0530

    'commit-9'

--- application.py
+++ application.py
@@ -34,5 +34,5 @@ def index():
     return render_template('index.html', prediction=None)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
 
